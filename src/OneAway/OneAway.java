34a89b8ff0b29e8b9b262766ebc678ccb757db5a
NhiCung <nhcung@knox.edu>
2019-07-05 22:42:33 -0500

    update

---
+++ src/OneAway/OneAway.java
@@ -0,0 +1,44 @@
+package OneAway;
+
+public class OneAway {
+	public static boolean oneEditAway(String first, String second) {
+		if (first.length() == second.length()) {
+			return oneReplace(first, second);
+		} else if (first.length() == second.length() + 1) {
+			return oneInsert(second, first);
+		} else if (first.length() == second.length() - 1) {
+			return oneInsert(first, second);
+		}
+		return false;
+	}
+
+	public static boolean oneReplace(String r1, String r2) {
+		boolean foundDifference = false;
+		for (int i = 1; i < r1.length(); i++) {
+			if (r1.charAt(i) != r2.charAt(i)) {
+				if (foundDifference) {
+					return false;
+				}
+				foundDifference = true;
+			}
+		}
+		return true;
+	}
+
+	public static boolean oneInsert(String s1, String s2) {
+		int index1 = 0;
+		int index2 = 0;
+		while (index1 < s1.length() && index2 < s2.length()) {
+			if (s1.charAt(index1) != s2.charAt(index2)) {
+				if (index1 != index2) {
+					return false;
+				}
+				index2++;
+			} else {
+				index1++;
+				index2++;
+			}
+		}
+		return true;
+	}
+}
---
+++ src/OneAway/OneAwayTest.java
@@ -0,0 +1,27 @@
+package OneAway;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+class OneAwayTest {
+
+	@Test
+	public void testOneEditAway() {
+		String s1 = "pale";
+		String s2 = "bale";
+		String s3 = "bae";
+		String s4 = "ple";
+		String s5 = "pales";
+
+		// OneAway away = new OneAway();
+		// away.
+
+		assertTrue(OneAway.oneEditAway(s1, s2));
+		assertTrue(OneAway.oneEditAway(s5, s1));
+		assertTrue(OneAway.oneEditAway(s1, s4));
+		assertFalse(OneAway.oneEditAway(s1, s3));
+
+	}
+
+}
