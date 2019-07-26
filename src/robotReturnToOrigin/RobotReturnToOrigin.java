186598cb861cbbbde10eb002e63c1e13dcbcfe2f
NhiCung <nhcung@knox.edu>
2019-07-25 23:23:30 -0500

    update

--- src/robotReturnToOrigin/RobotReturnToOrigin.java
+++ src/robotReturnToOrigin/RobotReturnToOrigin.java
@@ -1,32 +1,26 @@
 package robotReturnToOrigin;
 
 public class RobotReturnToOrigin {
-	public boolean JudgeCircle(String moves) {
-        int x =0; 
-        int y = 0;
-        for (int i = 0; i < moves.Length; i++){
-            if (moves[i] == 'R')
-            {
-                x++;
-            }
-            else if (moves[i] == 'L')
-            {
-                x--;
-            }
-            else if (moves[i] == 'U')
-            {
-                y++;
-            }
-            else if (moves[i] == 'D')
-            {
-                y--;
-            }
-        }
-        if ( x == 0 && y == 0)
-        {
-            return true;
-        }
-        
-        return false;
-    }  
+	public boolean judgeCircle(String moves) {
+
+		int x = 0;
+		int y = 0;
+		for (int i = 0; i < moves.length(); i++) {
+			if (moves.charAt(i) == 'R') {
+				x++;
+			} else if (moves.charAt(i) == 'L') {
+				x--;
+			} else if (moves.charAt(i) == 'U') {
+				y++;
+			} else if (moves.charAt(i) == 'D') {
+				y--;
+			}
+		}
+		if (x == 0 && y == 0) {
+			return true;
+		}
+
+		return false;
+
+	}
 }
