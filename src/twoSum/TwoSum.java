23aa72512ab2858757d86fd38bf3d0fc35549811
NhiCung <nhcung@knox.edu>
2019-07-25 23:21:31 -0500

    update

---
+++ src/twoSum/TwoSum.java
@@ -0,0 +1,22 @@
+package twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+	public int[] twoSum(int[] nums, int target) {
+	       int[] result = new int[2];
+	        Map<Integer, Integer> map = new HashMap<>();
+	        for (int i =0; i < nums.length; i ++){
+	            map.put(nums[i],i);
+	        }
+	        for (int i = 0; i < nums.length; i ++){
+	            int secondInt = target - nums[i];
+	            if (map.containsKey(secondInt) && map.get(secondInt) != i ){
+	                result[0] = map.get(secondInt);
+	                result[1] = i;
+	            }
+	        }
+	        return result;
+	    }
+}
