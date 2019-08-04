2ff518106f1a859873d920399b56829484458cab
NhiCung <nhcung@knox.edu>
2019-08-04 01:07:29 -0500

    update

--- src/MergeTwoSortedLists/mergeTwoSortedLists.java
+++ src/MergeTwoSortedLists/mergeTwoSortedLists.java
@@ -1,3 +1,18 @@
+/*
+ * DESCRIPTION:
+ * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+	Example:
+
+	Input: 1->2->4, 1->3->4
+	Output: 1->1->2->3->4->4
+	
+	RESULT:
+	Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Two Sorted Lists.
+	Memory Usage: 39.2 MB, less than 17.24% of Java online submissions for Merge Two Sorted Lists. 
+ * */
+
+
 package mergeTwoSortedLists;
 
 public class mergeTwoSortedLists {
