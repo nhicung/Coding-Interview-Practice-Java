e39d5b5af8f5f9e83ea3628f54d51e385a43b265
NhiCung <nhcung@knox.edu>
2019-08-06 23:12:50 -0500

    UPDATE

--- src/MoveZero/moveZero.java
+++ src/MoveZero/moveZero.java
@@ -1,3 +1,21 @@
+/*
+DESCRIPTION
+	Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+	
+	Example:
+	
+	Input: [0,1,0,3,12]
+	Output: [1,3,12,0,0]
+	Note:
+	
+	You must do this in-place without making a copy of the array.
+	Minimize the total number of operations.
+	
+MY SOLUTION
+	Runtime: 0 ms, faster than 100.00% of Java online submissions for Move Zeroes.
+	Memory Usage: 38.1 MB, less than 82.14% of Java online submissions for Move Zeroes.
+
+ */
 package moveZero;
 
 public class moveZero {
