7d64499c47d42707738650092cd7011f941ded77
NhiCung <nhcung@knox.edu>
2019-08-16 19:53:32 -0500

    update

--- src/Mumbling/Mumbling.java
+++ src/Mumbling/Mumbling.java
@@ -2,24 +2,25 @@ package mumbling;
 
 public class Mumbling {
 	public static String accum(String s) {
-	     // your code
-	     String str = "";
-	       for (int i=0; i<s.length(); i++){
-	         char rest = Character.toLowerCase(s.charAt(i));
-	         for (int j=0; j<i+1; j++){
-	           if (j==0){
-	             char first = Character.toUpperCase(s.charAt(i));
-	             str += first;
-	           }else{
-	           str += rest;
-	           }
-	           }
-	           if (i==s.length()-1){
-	            break;
-	           }else{
-	            str += "-";
-	           }  
-	      }
-	      return str;
-	    }
+		char rest;
+		char first;
+		String str = "";
+		for (int i = 0; i < s.length(); i++) {
+			rest = Character.toLowerCase(s.charAt(i));
+			for (int j = 0; j < i + 1; j++) {
+				if (j == 0) {
+					first = Character.toUpperCase(s.charAt(i));
+					str += first;
+				} else {
+					str += rest;
+				}
+			}
+			if (i == s.length() - 1) {
+				break;
+			} else {
+				str += "-";
+			}
+		}
+		return str;
+	}
 }
