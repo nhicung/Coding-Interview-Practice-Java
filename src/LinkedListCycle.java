b648a2d355278248997ef295385f3a70035a771f
NhiCung <nhcung@knox.edu>
2019-11-20 23:44:26 -0600

    UPDATE

--- src/LinkedListCycle.java
+++ src/LinkedListCycle.java
@@ -1,4 +1,37 @@
+/*
+PROBLEM DESCRIPTION:
+	Given a linked list, determine if it has a cycle in it.
+	
+	To represent a cycle in the given linked list, we use an integer pos which represents the position 
+	(0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the 
+	linked list.	
+	
+	Example 1:
+	
+	Input: head = [3,2,0,-4], pos = 1
+	Output: true
+	Explanation: There is a cycle in the linked list, where tail connects to the second node.
+	
+	Example 2:
+	
+	Input: head = [1,2], pos = 0
+	Output: true
+	Explanation: There is a cycle in the linked list, where tail connects to the first node.
+	
+	Example 3:
+	
+	Input: head = [1], pos = -1
+	Output: false
+	Explanation: There is no cycle in the linked list. 
+	
+	Follow up:
+	
+	Can you solve it using O(1) (i.e. constant) memory?
 
+MY SOLUTION:
+	Runtime: 4 ms, faster than 21.97% of Java online submissions for Linked List Cycle.
+	Memory Usage: 37.1 MB, less than 100.00% of Java online submissions for Linked List Cycle.
+ */
 
 import java.util.HashSet;
 import java.util.Set;
