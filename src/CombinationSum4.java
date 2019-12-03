3f9c82fcce4669bc74a708f27751ba831997b5c7
NhiCung <nhcung@knox.edu>
2019-12-03 11:31:38 +0700

    Update CombinationSum4.java

--- src/CombinationSum4.java
+++ src/CombinationSum4.java
@@ -1,28 +1,28 @@
 /*
 DESCRIPTION:
-Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
-
-Example:
-
-nums = [1, 2, 3]
-target = 4
-
-The possible combination ways are:
-(1, 1, 1, 1)
-(1, 1, 2)
-(1, 2, 1)
-(1, 3)
-(2, 1, 1)
-(2, 2)
-(3, 1)
-
-Note that different sequences are counted as different combinations.
-
-Therefore the output is 7.
+	Given an integer array with all positive numbers and no duplicates, find the number of possible combinations that add up to a positive integer target.
+	
+	Example:
+	
+	nums = [1, 2, 3]
+	target = 4
+	
+	The possible combination ways are:
+	(1, 1, 1, 1)
+	(1, 1, 2)
+	(1, 2, 1)
+	(1, 3)
+	(2, 1, 1)
+	(2, 2)
+	(3, 1)
+	
+	Note that different sequences are counted as different combinations.
+	
+	Therefore the output is 7.
 
 SOLUTION:
-Runtime: 1 ms, faster than 82.45% of Java online submissions for Combination Sum IV.
-Memory Usage: 34.2 MB, less than 100.00% of Java online submissions for Combination Sum IV.
+	Runtime: 1 ms, faster than 82.45% of Java online submissions for Combination Sum IV.
+	Memory Usage: 34.2 MB, less than 100.00% of Java online submissions for Combination Sum IV.
  */
 public class CombinationSum4 {
 	public int combinationSum4(int[] nums, int target) {
