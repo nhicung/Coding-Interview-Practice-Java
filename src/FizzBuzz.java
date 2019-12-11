689e4efabd743781f76bcff178e33554c79d7452
NhiCung <nhcung@knox.edu>
2019-12-11 12:04:07 +0700

    update

---
+++ src/FizzBuzz.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+/*
+DESCRIPTION:
+	Write a program that outputs the string representation of numbers from 1 to n.
+	
+	But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+	
+	Example:
+	
+	n = 15,
+	
+	Return:
+	[
+	    "1",
+	    "2",
+	    "Fizz",
+	    "4",
+	    "Buzz",
+	    "Fizz",
+	    "7",
+	    "8",
+	    "Fizz",
+	    "Buzz",
+	    "11",
+	    "Fizz",
+	    "13",
+	    "14",
+	    "FizzBuzz"
+	]
+SOLUTION:
+
+ */
+public class FizzBuzz {
+	public List<String> fizzBuzz(int n){
+		List<String> result = new ArrayList<String>();
+		for (int i = 0; i <= n; i++) {
+			if (i % 3 == 0) {
+				result.add("Fizz");
+			} else if ( i % 5 == 0) {
+				result.add("Buzz");
+			} else if (i % 15 ==0) {
+				result.add("FizzBuzz");
+			} else {
+				result.add(Integer.toString(i));
+			}
+		}
+		return result;
+	}
+}
