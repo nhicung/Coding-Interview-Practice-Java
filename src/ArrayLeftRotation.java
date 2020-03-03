45fed0c76ec77c0f9c0631cc44de8608bef964f4
Nhi Cung <nhcung@knox.edu>
2020-03-02 23:39:01 -0600

    Raay Problem: Solution

--- src/ArrayLeftRotation.java
+++ src/ArrayLeftRotation.java
@@ -1,3 +1,10 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 /*
  * A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become .
 Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
@@ -25,5 +32,56 @@ When we perform  left rotations, the array undergoes the following sequence of c
 
  */
 public class ArrayLeftRotation {
+	static int[] rotLeft(int[] a, int d) {
+        int[] result = new int[a.length];
+        List<Integer> rotList = new ArrayList<Integer>();
+        for (int i = d; i < a.length; i++){
+            rotList.add(a[i]);
+        }
+        for (int j = 0; j <d; j++){
+            rotList.add(a[j]);
+        }
+        for (int h = 0; h < result.length; h++){
+            result[h] = rotList.get(h);
+        }
+        return result;
+    }
 
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
 }
